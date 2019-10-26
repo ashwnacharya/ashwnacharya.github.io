4e05a94466e4525b44b921d778609f89b3500fe4
Ashwin Bilgi <asbilgi@microsoft.com>
2019-10-27 02:34:57 +0530

    Changes to deploy to new infra.

--- build_docker.sh
+++ build_docker.sh
@@ -2,6 +2,6 @@
 bundle exec jekyll build
 SHORT_SHA=`git rev-parse --short HEAD`
 docker build -t blog:$SHORT_SHA .
-docker tag blog:$SHORT_SHA ashwnacharya.azurecr.io/blog:$SHORT_SHA
-docker push ashwnacharya.azurecr.io/blog:$SHORT_SHA
+docker tag blog:$SHORT_SHA ashwnacharyaacr.azurecr.io/blog:$SHORT_SHA
+docker push ashwnacharyaacr.azurecr.io/blog:$SHORT_SHA
 
